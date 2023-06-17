db260450a3e58f6c1a69592131a8fbe6b1db4584
a <i.shumilov@arktech.ai>
2023-06-17 12:49:35 +0300

    upd

--- hook.py
+++ hook.py
@@ -31,5 +31,5 @@ def receive_update():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 80))
+    app.run(host="0.0.0.0", port=port, ssl_context='adhoc')
