c8c5c2cde84eba5fac5bdabe2d38432dcb2e92a3
a <i.shumilov@arktech.ai>
2023-06-25 20:31:01 +0300

    upd

--- handler.py
+++ handler.py
@@ -1,4 +1,10 @@
+import telebot
+from telebot import types, apihelper
+
+token = '6035518578:AAEkKUp9yai17ljTJUT1vVv3oEh6Zk9XVnU'
+bot = telebot.TeleBot(token)
 
 
 def handler(data: dict) -> None:
-    print(name)
+    if message['text'] == 'Hellow':
+        bot.send_message(data['chat']['id'], text='Hellow')
